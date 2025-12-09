e23a0d96c78130220197f1d07866aa036c39e4b1
Yosri27 <abdelrahmanyosery@gmail.com>
2025-12-09 08:16:23 +0200

    first commit

--- Index.js
+++ Index.js
@@ -1,150 +1,150 @@
+// // const path = require ('path');
 // const path = require ('path');
-const path = require ('path');
-const fs   = require('fs');
+// const fs   = require('fs');
 
-function One()
-{
+// function One()
+// {
    
-console.log(__dirname);
-console.log(__filename);
+// console.log(__dirname);
+// console.log(__filename);
     
-}
+// }
 
-function Two(){
-const xpath = "/user/files/report.pdf";
-y = path.parse(xpath);
-return(y.base);
-
-
-}
-function Three(){
+// function Two(){
+// const xpath = "/user/files/report.pdf";
+// y = path.parse(xpath);
+// return(y.base);
 
 
-y = path.format(xpath);
-return y;
-
-}
-function Four(){
-
-    const xpath = "/docs/readme.md";
-
-x = path.parse(xpath);
-return(x.ext);
+// }
+// function Three(){
 
 
-}
+// y = path.format(xpath);
+// return y;
 
-function five(){
+// }
+// function Four(){
 
-const xpath = "/home/app/main.js";
+//     const xpath = "/docs/readme.md";
 
-x = path.parse(xpath);
-
-return {
-    Name: x.name,
-    Ext:  x.ext
-
-}
-}
-function six(){
-
-const xpath = "/home/user/file.txt";
-
-x = path.isAbsolute(xpath);
-return x
-}
-function seven(...k){
+// x = path.parse(xpath);
+// return(x.ext);
 
 
-x = path.join(...k)
-return x
-}
-function eight(){
+// }
 
-const xpath = "./index.js";
-x = path.resolve(xpath)
-return x
-}
+// function five(){
 
-function nine(...k){
+// const xpath = "/home/app/main.js";
+
+// x = path.parse(xpath);
+
+// return {
+//     Name: x.name,
+//     Ext:  x.ext
+
+// }
+// }
+// function six(){
+
+// const xpath = "/home/user/file.txt";
+
+// x = path.isAbsolute(xpath);
+// return x
+// }
+// function seven(...k){
 
 
-x = path.join(...k)
-return x
-}
-function ten(){
- const xpath = "C:/Users/20102/Desktop/node js/file.txt";
-      fs.access(xpath,  (err) => {
-    if (err) {
-      console.log("File does not exist");
-    } else {
-      fs.unlink(xpath, (err) => {
-        if (err) {
-          console.log("Error deleting file:", err);
-        } else {
-          console.log("File deleted successfully");
-        }
-      });
-    }
- });
-}
+// x = path.join(...k)
+// return x
+// }
+// function eight(){
 
- function elven(){
+// const xpath = "./index.js";
+// x = path.resolve(xpath)
+// return x
+// }
+
+// function nine(...k){
+
+
+// x = path.join(...k)
+// return x
+// }
+// function ten(){
+//  const xpath = "C:/Users/20102/Desktop/node js/file.txt";
+//       fs.access(xpath,  (err) => {
+//     if (err) {
+//       console.log("File does not exist");
+//     } else {
+//       fs.unlink(xpath, (err) => {
+//         if (err) {
+//           console.log("Error deleting file:", err);
+//         } else {
+//           console.log("File deleted successfully");
+//         }
+//       });
+//     }
+//  });
+// }
+
+//  function elven(){
  
- events.on("log",(msg)=>
-        {
+//  events.on("log",(msg)=>
+//         {
 
-            console.log(msg);
+//             console.log(msg);
             
             
-        })
-        events.emit("log","Welcome event triggered!")
- }
+//         })
+//         events.emit("log","Welcome event triggered!")
+//  }
 
-const {EventEmitter} = require('events');
-const events = new EventEmitter()
+// const {EventEmitter} = require('events');
+// const events = new EventEmitter()
 
- function triteen(){
+//  function triteen(){
     
-    events.on("log",(msg)=>
-        {
+//     events.on("log",(msg)=>
+//         {
 
-            return console.log("User Logged in",msg);
+//             return console.log("User Logged in",msg);
             
-        })
-        events.emit("log","Ahmed")
+//         })
+//         events.emit("log","Ahmed")
 
     
-}
+// }
 
- function fifteen(){
+//  function fifteen(){
     
-  xpath ="C:/Users/20102/Desktop/node js/file.txt";
-    fs.writeFile(xpath, '7amo', 'utf-8', (err) => {
-    if (err) {
-      console.log(err);
-    } else {
-      console.log("ok");
-    }
-  });
-}
+//   xpath ="C:/Users/20102/Desktop/node js/file.txt";
+//     fs.writeFile(xpath, '7amo', 'utf-8', (err) => {
+//     if (err) {
+//       console.log(err);
+//     } else {
+//       console.log("ok");
+//     }
+//   });
+// }
 
- function sixteen(){
+//  function sixteen(){
     
-    xpath="C:/Users/20102/Desktop/node js/file.txt";
+//     xpath="C:/Users/20102/Desktop/node js/file.txt";
 
-    y = fs.existsSync(xpath);
-    return y; 
- }
+//     y = fs.existsSync(xpath);
+//     return y; 
+//  }
 
-const os =  require('os')
+// const os =  require('os')
 
- function Seventeen(){
+//  function Seventeen(){
  
     
 
-console.log(`OS Platform: ${os.platform()}`);
+// console.log(`OS Platform: ${os.platform()}`);
 
-console.log(`CPU Architecture: ${os.arch()}`);
- }
+// console.log(`CPU Architecture: ${os.arch()}`);
+//  }
     
